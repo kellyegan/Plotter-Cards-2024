2999a803922e7845d8451eff57627bab9345caf3
Kelly Egan <kelly.egan@gmail.com>
2024-12-03 11:24:24 -0500

    Draw x

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 600);
 }
 
 function draw() {
   background(220);
+  line(20, 20, width - 20, height - 20);
+  line(20, height - 20, width - 20, 20);
 }
