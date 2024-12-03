a23c2c2db0529225c9f0f05329ede6e06555ab3a
Kelly Egan <kelly.egan@gmail.com>
2024-12-03 11:32:54 -0500

    Add plotSVG lib

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 function setup() {
   createCanvas(400, 600);
-}
-
-function draw() {
   background(220);
+
+  beginRecordSVG(this, "hello.svg");
   line(20, 20, width - 20, height - 20);
   line(20, height - 20, width - 20, 20);
+  endRecordSVG();
+
+  noLoop();
 }
