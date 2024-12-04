e54f14176ad785d7c2d75ece65949900eb09d739
Kelly Egan <kelly.egan@gmail.com>
2024-12-04 14:23:06 -0500

    Set up stub for matrix multiplication

--- sketch.js
+++ sketch.js
@@ -2,10 +2,26 @@ function setup() {
   createCanvas(400, 600);
   background(220);
 
-  beginRecordSVG(this, "hello.svg");
-  line(20, 20, width - 20, height - 20);
-  line(20, height - 20, width - 20, 20);
-  endRecordSVG();
+  // beginRecordSVG(this, "hello.svg");
+  // line(20, 20, width - 20, height - 20);
+  // line(20, height - 20, width - 20, 20);
+  // endRecordSVG();
 
-  noLoop();
+  // noLoop();
+
+  let projection = [
+    [1, 0, 0],
+    [0, 1, 0],
+  ];
+
+  let point = [[100, 75, 50]];
+
+  let projectedPoint = matmul(projection, point);
+
+  console.log(projectedPoint);
+}
+
+function matmul(matA, matB) {
+  let result = [];
+  return result;
 }
