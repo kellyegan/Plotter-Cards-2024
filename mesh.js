53dc5665a9c1721eaa032398f0564c849aa1efaf
Kelly Egan <kelly.egan@gmail.com>
2024-12-11 16:44:59 -0500

    Add edge sort method to Mesh

--- mesh.js
+++ mesh.js
@@ -23,6 +23,16 @@ class Mesh {
     return copy;
   }
 
+  sortEdges() {
+    this.edges.sort((a, b) => {
+      if (a[0] !== b[0]) {
+        return a[0] - b[0];
+      } else {
+        return a[1] - b[1];
+      }
+    });
+  }
+
   addTransformation(transformation) {
     this.transform = math.multiply(this.transform, transformation);
   }
