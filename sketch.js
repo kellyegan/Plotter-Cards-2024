6d195a53d8089ddae01743d53449a893e8c5cc9c
Kelly Egan <kelly.egan@gmail.com>
2024-12-13 14:39:21 -0500

    tweaking

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let scene;
 
-let eyeSpacing = 0.04;
+let eyeSpacing = 0.06;
 
 let leftCamera;
 let rightCamera;
@@ -19,6 +19,10 @@ let createSVG = false;
 
 let xAngle, yAngle, xStart, yStart;
 
+let bgModelTransforms;
+
+let spiralMesh;
+
 function preload() {}
 
 function setup() {
@@ -30,6 +34,7 @@ function setup() {
   yStart = 0;
 
   models = [
+    "Spiral1",
     "Solids-Bauble1",
     "Solids-Bauble2",
     "Solids-Cube",
@@ -51,6 +56,11 @@ function setup() {
     "Solids-TruncatedDodecahedronStar2",
   ];
 
+  // Generate spiral count, radius1, radius2, height, twist
+  spiralMesh = generateSpiral(48, 0.5, 0.5, 1);
+
+  // setBackgroundTransforms();
+
   modelIndex = 0;
   loadJSONModel(modelIndex);
 
@@ -60,7 +70,7 @@ function setup() {
   scene = new Scene();
 
   stroke(255);
-  strokeWeight(2);
+  strokeWeight(1);
 
   // saveJSON(modelMesh, "json/Solids-TruncatedDodecahedron.json");
 
@@ -80,14 +90,33 @@ function draw() {
     // angle += 0.005;
     scene.reset();
 
+    // bgModelTransforms.forEach((t, i) => {
+    //   scene.pushMatrix();
+    //   scene.translate(0, 0, t[3]);
+    //   scene.rotateZ(t[4] + (i * TAU) / bgModelTransforms.length);
+    //   scene.translate(3.5, 0, 0);
+    //   scene.rotateX(t[0]);
+    //   scene.rotateY(t[1]);
+    //   scene.rotateZ(t[2]);
+    //   scene.add(currentModelMesh);
+    //   scene.popMatrix();
+    // });
+
+    // scene.pushMatrix();
+    // scene.translate(0, 0, -1.5);
+    // scene.rotateX(PI / 2);
+    //
+    // scene.popMatrix();
+
     if (!paused) {
       xAngle = map(mouseY, 0, height, -PI / 2, PI / 2);
       yAngle = map(mouseX, 0, width, 0, PI);
     }
 
+    // scene.translate(0, 0, -8);
     scene.rotateX(xAngle);
     scene.rotateY(yAngle);
-
+    const s = 3;
     scene.add(currentModelMesh);
 
     //Stereo render
@@ -134,6 +163,7 @@ function keyPressed() {
       break;
     case "l":
     case "L":
+      j;
       showLabels = !showLabels;
       break;
     case "j":
@@ -141,9 +171,9 @@ function keyPressed() {
       saveJSON(currentModelMesh, currentModelName);
       break;
     case "d":
-      currentModelMesh.deleteVertex([
-        123, 128, 133, 138, 135, 140, 144, 70, 72, 66, 68, 64,
-      ]);
+      break;
+    case " ":
+      setBackgroundTransforms();
       break;
   }
 }
@@ -181,4 +211,32 @@ function onJSONLoaded(modelData) {
 function onJSONFailed(error) {
   console.log("Failed to load JSON. Loading STL...");
   loadSTLModel(modelIndex);
+}
+
+function generateSpiral(count = 20, r1 = 0.5, r2 = 0.5, h = 1, t = 2) {
+  const a = TAU / count;
+  const spiral = new Mesh();
+
+  for (let i = 0; i < count; i++) {
+    spiral.vertices.push([r1 * sin(a * i), -h / 2, r2 * cos(a * i), 1]);
+    spiral.vertices.push([r1 * sin(t + a * i), h / 2, r2 * cos(t + a * i), 1]);
+    spiral.edges.push([spiral.vertices.length - 2, spiral.vertices.length - 1]);
+  }
+
+  console.log(spiral);
+
+  return spiral;
+}
+
+function setBackgroundTransforms(count = 9) {
+  bgModelTransforms = [];
+  for (let i = 0; i < count; i++) {
+    bgModelTransforms.push([
+      random(0, TAU),
+      random(0, TAU),
+      random(0, TAU),
+      random(-8, -12),
+      random(-PI / 16, PI / 16),
+    ]);
+  }
 }
